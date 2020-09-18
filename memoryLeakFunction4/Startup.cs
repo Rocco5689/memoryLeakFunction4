c9109d1b0fbfe4feaad8bfd3066525085efe0aa8
Matthew Cavallo <matt.cavallo@outlook.com>
2020-09-18 18:01:11 -0500

    Add project files.

---
+++ memoryLeakFunction4/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System.Runtime;
+
+[assembly: FunctionsStartup(typeof(memoryLeakFunction4.Startup))]
+
+namespace memoryLeakFunction4
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            GCSettings.LatencyMode = GCLatencyMode.LowLatency;
+        }
+    }
+}
